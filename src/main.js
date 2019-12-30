d8c702be1a7179fcfb275bd6189f4a8249f8fe6b
ajycc20 <ajycc20@qq.com>
2019-12-30 11:02:42 +0800

    fix: revert the last commit

--- src/main.js
+++ src/main.js
@@ -15,6 +15,10 @@ import router from './router'
 import '@/icons' // icon
 import '@/permission' // permission control
 
+import db from './utils/localstorage'
+
+Vue.use(db)
+
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
