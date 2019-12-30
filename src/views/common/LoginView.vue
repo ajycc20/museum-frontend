ab6ae715f807d4fbaff54bea4203f0faddaff468
ajycc20 <ajycc20@qq.com>
2019-12-30 10:36:04 +0800

    fix: fix login router & add logo

--- src/views/common/LoginView.vue
+++ src/views/common/LoginView.vue
@@ -122,7 +122,7 @@ export default {
         if (valid) {
           this.loading = true
           this.$store.dispatch('user/login', this.loginForm).then(() => {
-            this.$router.push({ path: this.redirect || '/' })
+            this.$router.push({ path: '/dashboard' })
             this.loading = false
           }).catch(() => {
             this.loading = false
